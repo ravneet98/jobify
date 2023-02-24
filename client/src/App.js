3c7b46a9936247ba15d85a09de32078452ece7b3
Ravneet Singh <ravneet0298@gmail.com>
2023-02-24 08:49:05 -0500

    Bug fix for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   SharedLayout,
   Stats,
 } from "./pages/dashboard";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
